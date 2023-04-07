9e4edad65c8ff8792a832206bc9ff904e2269088
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2023-04-07 13:19:45 +0200

    block movement restrictions

--- Catherine Simulation/Assets/Scripts/Player/BlockInteractController.cs
+++ Catherine Simulation/Assets/Scripts/Player/BlockInteractController.cs
@@ -9,42 +9,54 @@ namespace Player
         private readonly Transform _transform;
         private readonly PlayerState _playerState;
         private readonly Inputs _input;
-        
-        
+
+
         public BlockInteractController(Transform playerTransform, PlayerState playerState, Inputs input)
         {
             _transform = playerTransform;
             _playerState = playerState;
             _input = input;
         }
-        
+
         /*
          * Pulls the block in front of the player, if any
          */
         public void MoveBlocks()
         {
-            if (_input.Pull() && _playerState.CanMoveBlocks())
+            if (_input.Pull() && _playerState.CanMoveBlocks() && !IsBlockBehindPlayer())
             {
                 BlockSolidController blockSolidController = GetBlockController();
                 if (blockSolidController == null) return;
-                
+
                 blockSolidController.TriggerPull(_transform, _playerState);
             }
-            else if (_input.Push() && _playerState.CanMoveBlocks())
+            else if (_input.Push() && _playerState.CanMoveBlocks() && !IsBlockBehindBlockInFront())
             {
                 BlockSolidController blockSolidController = GetBlockController();
                 if (blockSolidController == null) return;
-                
+
                 blockSolidController.TriggerPush(_transform, _playerState);
             }
         }
 
         private BlockSolidController GetBlockController()
         {
-            GameObject block = Level.GetBlock(_transform.position + Vector3.up + _playerState.GetDirection() * Level.BlockScale);
+            GameObject block =
+                Level.GetBlock(_transform.position + Vector3.up + _playerState.GetDirection() * Level.BlockScale);
             return block == null ? null : block.GetComponent<BlockSolidController>();
         }
-        
-        
+
+        private bool IsBlockBehindPlayer()
+        {
+            return Level.GetBlockInt(_transform.position - _playerState.GetDirection() * Level.BlockScale +
+                                     Vector3.up) !=
+                   Level.EmptyBlock;
+        }
+
+        private bool IsBlockBehindBlockInFront()
+        {
+            return Level.GetBlockInt(_transform.position + _playerState.GetDirection() * (2 * Level.BlockScale) +
+                                     Vector3.up) != Level.EmptyBlock;
+        }
     }
 }
