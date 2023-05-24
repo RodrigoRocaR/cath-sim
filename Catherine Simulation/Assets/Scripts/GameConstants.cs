736c56c2cad8db2e10bec90288b6a478c1d1308b
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2023-05-24 18:54:44 +0200

    move GameConstants.cs to global namespace

---
+++ Catherine Simulation/Assets/Scripts/GameConstants.cs
@@ -0,0 +1,16 @@
+﻿public static class GameConstants
+{
+    public const int BlockScale = 2;
+    public const int EmptyBlock = -1;
+    public const int SolidBlock = 0;
+    public const int VictoryBlock = 1;
+    public const int ImmovableBlock = 2;
+
+    public const string SolidBlockName = "BlockSolid";
+    public const string VictoryBlockName = "BlockVictory";
+    public const string ImmovableBlockName = "BlockImmovable";
+    public const string TimerTextName = "TextTimer";
+    public const string TimerDisplayOnCornerName = "TimerDisplay";
+
+    public const float GameOverMinimumY = -20;
+}
