80d1ad6320afb3cd854da32fdd2f275e62193fe5
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2023-06-15 18:34:44 +0200

    add custom priority queue

---
+++ Catherine Simulation/Assets/Scripts/Tools/DS/PriorityQueue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tools.DS
+{
+
+    public class PriorityQueue<T>
+    {
+        private List<T> _heap;
+        private IComparer<T> _comparer;
+
+        public int Count => _heap.Count;
+
+        public PriorityQueue() : this(Comparer<T>.Default)
+        {
+        }
+
+        public PriorityQueue(IComparer<T> comparer)
+        {
+            _heap = new List<T>();
+            _comparer = comparer;
+        }
+
+        public void Enqueue(T item)
+        {
+            _heap.Add(item);
+            int currentIndex = _heap.Count - 1;
+
+            while (currentIndex > 0)
+            {
+                int parentIndex = (currentIndex - 1) / 2;
+
+                if (_comparer.Compare(_heap[currentIndex], _heap[parentIndex]) >= 0)
+                    break;
+
+                Swap(currentIndex, parentIndex);
+                currentIndex = parentIndex;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (_heap.Count == 0)
+                throw new InvalidOperationException("Priority queue is empty.");
+
+            T firstItem = _heap[0];
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            int currentIndex = 0;
+
+            while (true)
+            {
+                int leftChildIndex = currentIndex * 2 + 1;
+                int rightChildIndex = currentIndex * 2 + 2;
+
+                if (leftChildIndex > lastIndex)
+                    break;
+
+                int smallerChildIndex = leftChildIndex;
+
+                if (rightChildIndex <= lastIndex && _comparer.Compare(_heap[rightChildIndex], _heap[leftChildIndex]) < 0)
+                    smallerChildIndex = rightChildIndex;
+
+                if (_comparer.Compare(_heap[currentIndex], _heap[smallerChildIndex]) <= 0)
+                    break;
+
+                Swap(currentIndex, smallerChildIndex);
+                currentIndex = smallerChildIndex;
+            }
+
+            return firstItem;
+        }
+
+        public T Peek()
+        {
+            if (_heap.Count == 0)
+                throw new InvalidOperationException("Priority queue is empty.");
+
+            return _heap[0];
+        }
+
+        private void Swap(int indexA, int indexB)
+        {
+            (_heap[indexA], _heap[indexB]) = (_heap[indexB], _heap[indexA]);
+        }
+    }
+}
---
+++ Catherine Simulation/Assets/Tests/EditMode/Tools/DS/PriorityQueue.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Tools.DS;
+
+namespace Tests.EditMode.Tools.DS
+{
+    public class PriorityQueue
+    {
+
+        [Test]
+        public void TestEnqueueDequeue()
+        {
+            var pq = new PriorityQueue<int>();
+            pq.Enqueue(6);
+            pq.Enqueue(3);
+            pq.Enqueue(10);
+
+            Assert.AreEqual(3, pq.Dequeue());
+            Assert.AreEqual(6, pq.Dequeue());
+            Assert.AreEqual(10, pq.Dequeue());
+        }
+        
+        [Test]
+        public void TestPeek()
+        {
+            var pq = new PriorityQueue<int>();
+            pq.Enqueue(6);
+            pq.Enqueue(3);
+            pq.Enqueue(10);
+
+            Assert.AreEqual(3, pq.Peek());
+            Assert.AreEqual(3, pq.Dequeue());
+        }
+    }
+}
