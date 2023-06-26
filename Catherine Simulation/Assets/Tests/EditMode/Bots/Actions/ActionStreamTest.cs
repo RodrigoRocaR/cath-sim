471a27ac2a6cc46c75619305fad323f4fffaff34
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2023-06-26 22:02:03 +0200

    improve test coverage ActionStreamTest.cs

--- Catherine Simulation/Assets/Tests/EditMode/Bots/Actions/ActionStreamTest.cs
+++ Catherine Simulation/Assets/Tests/EditMode/Bots/Actions/ActionStreamTest.cs
@@ -49,25 +49,101 @@ namespace Tests.EditMode.Bots.Actions
             List<(int, int)> examplePositions = new List<(int, int)>
             {
                 (0, 0), // x, z
-                (0, 1),
-                (1, 1),
                 (1, 0),
+                (1, 1),
+                (0, 1),
                 (0, 0)
             };
 
             List<Action> expectedMovements = new List<Action>
             {
-                Action.Forward,
-                Action.Jump,
                 Action.Right,
-                Action.Backward,
+                Action.Jump,
+                Action.Forward,
                 Action.Left,
+                Action.Backward,
                 Action.Jump
             };
             Level2D level2D = new Level2D(new[]
             {
                 new [] { 0, 1},
                 new [] { 1, 1},
+            });
+
+            ActionStream actionStream = new ActionStream(level2D);
+            actionStream.CreateFromPositions(examplePositions);
+            
+            var movements = actionStream.GetAsList();
+            int n = movements.Count;
+
+            for (int i=0; i<n; i++)
+            {
+                Assert.AreEqual(expectedMovements[i], movements[i]);
+            }
+        }
+        
+        [Test]
+        public void TestDoesNotAddExtraJumps()
+        {
+            List<(int, int)> examplePositions = new List<(int, int)>
+            {
+                (0, 0), // x, z
+                (0, 1),
+                (0, 2),
+                (0, 3),
+                (0, 4),
+                (0, 5),
+            };
+
+            List<Action> expectedMovements = new List<Action>
+            {
+                Action.Forward,
+                Action.Forward,
+                Action.Forward,
+                Action.Jump,
+                Action.Forward,
+                Action.Jump,
+                Action.Forward
+            };
+            Level2D level2D = new Level2D(new[]
+            {
+                new [] { 0, 0, 0, 1, 0, 0, 3, 4, 5}
+            });
+
+            ActionStream actionStream = new ActionStream(level2D);
+            actionStream.CreateFromPositions(examplePositions);
+            
+            var movements = actionStream.GetAsList();
+            int n = movements.Count;
+
+            for (int i=0; i<n; i++)
+            {
+                Assert.AreEqual(expectedMovements[i], movements[i]);
+            }
+        }
+        
+        [Test]
+        public void TestJumpsDown()
+        {
+            List<(int, int)> examplePositions = new List<(int, int)>
+            {
+                (0, 0), // x, z
+                (0, 1),
+                (0, 2),
+                (0, 3)
+            };
+
+            List<Action> expectedMovements = new List<Action>
+            {
+                Action.Forward,
+                Action.Jump,
+                Action.Forward,
+                Action.Jump,
+                Action.Forward
+            };
+            Level2D level2D = new Level2D(new[]
+            {
+                new [] { 0, 1, 0, 0, 0, 4}
             });
 
             ActionStream actionStream = new ActionStream(level2D);
