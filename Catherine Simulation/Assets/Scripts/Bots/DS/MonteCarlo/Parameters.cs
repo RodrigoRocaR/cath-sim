95979bb5e431adecf163591aa003eb9eab747a02
RodrigoRocaR <61027147+RodrigoRocaR@users.noreply.github.com>
2023-07-10 19:38:27 +0200

    update params

--- Catherine Simulation/Assets/Scripts/Bots/DS/MonteCarlo/Parameters.cs
+++ Catherine Simulation/Assets/Scripts/Bots/DS/MonteCarlo/Parameters.cs
@@ -4,5 +4,6 @@
     {
         public const int C = 2; // Balance between exploration and exploitation
         public const int RolloutDepth = 10; // Nodes to be explored when a rollout happens
+        public const int MaxIterations = 25; // To prevent the game from crashing or requesting too much memory
     }
 }
